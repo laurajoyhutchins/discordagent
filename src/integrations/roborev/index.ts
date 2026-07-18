6d00e137c48ec881e3d1f43300f9fb694ccc5a4a
Laura <laurajoyhutchins@gmail.com>
2026-07-18 15:01:25 -0600

    feat: export RoboRev lifecycle signal

--- src/integrations/roborev/index.ts
+++ src/integrations/roborev/index.ts
@@ -2,4 +2,5 @@ export { createRoborevReviewSource } from './roborevReviewSource.js';
 export { buildReviewEmbed, anyProjectHasRoborev, hasRoborevSetup } from './roborevRenderer.js';
 export { isCliAvailable as isRoborevCliAvailable } from './roborevCli.js';
 export { matchProject, normalizeToNotification } from './roborevEventParser.js';
+export { notifyRoborevConfigurationChanged } from './roborevLifecycle.js';
 export type { RoborevStreamEvent } from './types.js';
